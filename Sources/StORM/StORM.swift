d74cc15967b660bd71acd900a2d65b6bc4c43373
Tristan Leblanc <tristan@moosefactory.eu>
2017-06-13 10:29:58 +0200

    Added mirroredProperties property, to manually specify properties ( from class and superclasses ) to mirror

--- Sources/StORM/StORM.swift
+++ Sources/StORM/StORM.swift
@@ -15,24 +15,38 @@ public var StORMdebug = false
 /// Base StORM superclass from which all Database-Connector StORM classes inherit.
 /// Provides base functionality and rules.
 open class StORM {
-
+	
 	/// Results container of type StORMResultSet.
 	open var results		= StORMResultSet()
-
+	
 	/// connection error status of type StORMError.
 	open var error			= StORMError()
-
+	
 	/// Contain last error message as string.
 	open var errorMsg		= ""
-
+	
+	/// Returns the class mirror
+	/// If mirroredProperties is defined, returns specified properties in this class, including ancestors.
+	open var mirror: Mirror {
+		get {
+			let props = self.mirroredProperties()
+			return  props != nil ? Mirror(self, children: props!, ancestorRepresentation: Mirror.AncestorRepresentation.generated) : Mirror(reflecting: self)
+		}
+	}
+	
+	/// The properties to include in mirroring operations
+	open func mirroredProperties() -> DictionaryLiteral<String, Any>? {
+		return nil;
+	}
+	
 	/// Base empty init function.
 	public init() {}
-
+	
 	/// Provides structure introspection to client methods.
 	public func cols(_ offset: Int = 0) -> [(String, Any)] {
 		var c = [(String, Any)]()
 		var count = 0
-		let mirror = Mirror(reflecting: self)
+		let mirror = self.mirror
 		for child in mirror.children {
 			guard let key = child.label else {
 				continue
@@ -45,13 +59,13 @@ open class StORM {
 		}
 		return c
 	}
-
+	
 	/// Returns a [(String,Any)] object representation of the current object.
 	/// If any object property begins with an underscore, or with "internal_" it is omitted from the response.
 	public func asData(_ offset: Int = 0) -> [(String, Any)] {
 		var c = [(String, Any)]()
 		var count = 0
-		let mirror = Mirror(reflecting: self)
+		let mirror = self.mirror
 		for case let (label?, value) in mirror.children {
 			if count >= offset && !label.hasPrefix("internal_") && !label.hasPrefix("_") {
 				if value is [String:Any] {
@@ -66,13 +80,13 @@ open class StORM {
 		}
 		return c
 	}
-
+	
 	/// Returns a [String:Any] object representation of the current object.
 	/// If any object property begins with an underscore, or with "internal_" it is omitted from the response.
 	public func asDataDict(_ offset: Int = 0) -> [String: Any] {
 		var c = [String: Any]()
 		var count = 0
-		let mirror = Mirror(reflecting: self)
+		let mirror = self.mirror
 		for case let (label?, value) in mirror.children {
 			if count >= offset && !label.hasPrefix("internal_") && !label.hasPrefix("_") {
 				if value is [String:Any] {
@@ -87,17 +101,17 @@ open class StORM {
 		}
 		return c
 	}
-
+	
 	/// Returns a tuple of name & value of the object's key
 	/// The key is determined to be it's first property, which is assumed to be the object key.
 	public func firstAsKey() -> (String, Any) {
-		let mirror = Mirror(reflecting: self)
+		let mirror = self.mirror
 		for case let (label?, value) in mirror.children {
 			return (label, value)
 		}
 		return ("id", "unknown")
 	}
-
+	
 	/// Returns a boolean that is true if the first property in the class contains a value.
 	public func keyIsEmpty() -> Bool {
 		let (_, val) = firstAsKey()
@@ -115,7 +129,7 @@ open class StORM {
 			}
 		}
 	}
-
+	
 	/// The create method is designed to be overridden
 	/// If not set in the chile class it will return an error of the enum value .notImplemented
 	open func create() throws {
@@ -123,4 +137,3 @@ open class StORM {
 	}
 	
 }
-
